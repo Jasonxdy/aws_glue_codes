2720ef55797ac25976155a7137a2b49bbcda86f1
Jasonxdy <dicobomb@naver.com>
2022-11-10 13:16:16 +0900

    dd

--- glue_job_oracle_db.py
+++ glue_job_oracle_db.py
@@ -1,4 +1,5 @@
-from pyspark.context import SparkContext  
+from pyspark.context import SparkContext
+from awsglue.utils import getResolvedOptions 
 from awsglue.context import GlueContext  
 from awsglue.dynamicframe import DynamicFrame  
 from awsglue.transforms import *  
@@ -6,41 +7,33 @@ import pyspark.sql.functions as F
 from pyspark.sql.functions import col
 from pyspark.sql.types import StringType,BooleanType,DateType
 import boto3
+import sys
+
+CONNECT_NAME='ERP_connect'
 
 client = boto3.client('glue')
-response = client.get_connection(Name='ERP_connect')
+response = client.get_connection(Name=CONNECT_NAME)
 DB_URL=response['Connection']['ConnectionProperties']['JDBC_CONNECTION_URL']
 DB_USERNAME=response['Connection']['ConnectionProperties']['USERNAME']
 DB_PASSWORD=response['Connection']['ConnectionProperties']['PASSWORD']
+
 
 sc = SparkContext() 
 glueContext = GlueContext(sc) 
 spark = glueContext.spark_session
 
-#ERP_connect
-query = f"(SELECT * FROM MTL_CATEGORY_SETS_VL) a"  
 
-# MPRM_connect
-# query = f"(SELECT * FROM CUST_INFO) a"  
+tb_nm=['RCV_TRANSACTIONS']
 
-
-df = spark.read.format("jdbc").option("url", DB_URL).option("user", DB_USERNAME).option("password", DB_PASSWORD).option("dbtable", query).load()
-print((df.count(), len(df.columns)))
-print('-------------')
-
-# for col in df.dtypes:
-#     print(col[0]+" , "+col[1])
+for TABLE_NAME in tb_nm:
+    query = f"(SELECT TRANSACTION_TYPE,TRANSACTION_DATE,SHIPMENT_HEADER_ID,SHIPMENT_LINE_ID,SOURCE_DOCUMENT_CODE,UOM_CODE,PO_HEADER_ID,PO_RELEASE_ID,PO_LINE_ID,PO_LINE_LOCATION_ID,VENDOR_ID,VENDOR_SITE_ID,ORGANIZATION_ID,SUBINVENTORY,LOCATOR_ID FROM {TABLE_NAME}) a"  
+    # print (query)
     
-# df = df.withColumn('movieId', col('movieId').cast(StringType()))
-
-
-# for col in df.dtypes:
-#     print(col[0]+" , "+col[1])
-
-# df.write.format('parquet').mode('overwrite').option("compression","gzip").save('s3://dbc-hands-on-bucket-aileen/test/')  
-
-# df = spark.read.format("parquet").load('s3://dbc-hands-on-bucket-aileen/test/part-00000-08b6acd2-a174-4806-9642-3486bb640efd-c000.gz.parquet')
-
-# for col in df.dtypes:
-#     print(col[0]+" , "+col[1])
+    df = spark.read.format("jdbc").option("url", DB_URL).option("user", DB_USERNAME).option("password", DB_PASSWORD).option("dbtable", query).load()
+    # print(df.head(10))
+    print((df.count(), len(df.columns)))
+    print('-------------')
+    
+    df.write.format('csv').option("compression","gzip").partitionBy('TRANSACTION_DATE').save(f"s3://hl-sf-poc/ods/{TABLE_NAME}/",header='true')
+    print ('successed')
 
